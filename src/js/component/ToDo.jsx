4a57e87c887ce9ff9283f51d7893285ecb27ecd6
BethaSL <90810803+BethaSL@users.noreply.github.com>
2021-11-15 21:12:30 -0400

    To do

---
+++ src/js/component/ToDo.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+
+//TO DO LIST WITH FETCH
+//https://assets.breatheco.de/apis/fake/todos/
+//https://reqbin.com/
+
+function ToDo() {
+	//useStates
+	const [tasks, setTasks] = useState([]); //tasks list
+	const [task, setTask] = useState(""); //single task
+	//const [xButton, setxButton] = useState(-1); //the x to delete tasks
+
+	//Functions
+	const handleTask = event => {
+		setTask(event.target.value);
+	};
+	const handleSubmit = event => {
+		event.preventDefault();
+	};
+	const handleKeyPress = event => {
+		if (event.key == "Enter") {
+			if (task != "") {
+				setTasks([...tasks, event.target.value]);
+				setTask("");
+			}
+		}
+	};
+	const handleDeleteTask = id => {
+		const newToDo = tasks.filter((task, index) => index != id);
+		setTasks(newToDo);
+	};
+
+	//Body
+	return (
+		<div className="container">
+			<div className="row justify-content-center mt-2">
+				<div className="col-12 col-md-6">
+					<h1>
+						<strong> To do list </strong>
+					</h1>
+					<form onSubmit={handleSubmit}>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="What needs to be done?"
+							onChange={handleTask}
+							onKeyPress={handleKeyPress}
+							value={task}
+						/>
+					</form>
+				</div>
+			</div>
+			<div className="row justify-content-center" id="tasksBox">
+				<div className="col-12 col-md-6">
+					<ul>
+						{tasks.map((task, index) => {
+							return (
+								<li
+									key={index}
+									onClick={() => handleDeleteTask(index)}
+									/*className={
+										xButton === index
+											? "fas fa-times"
+											: "noInTask"
+									}*/
+								>
+									{task}
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<div className="row justify-content-center" id="taskCounter">
+					{tasks.length == 1
+						? `${tasks.length} item left`
+						: `${tasks.length} items left`}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ToDo;
